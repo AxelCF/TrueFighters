43f3be7487760acc6375dbf8666d61308deb63a6
AxelCF <axelmaurice.1997@gmail.com>
2022-11-07 07:53:04 +0100

    trailwind twitch

--- resources/views/stream/twitch.blade.php
+++ resources/views/stream/twitch.blade.php
@@ -1,30 +1,29 @@
 <x-app-layout>      
-    <div class="bg-neutral-800 flex flex-wrap justify-around ">
-        <div class="flex flex-col mt-20 overflow-x-auto scrollbar-hide">
-            <div>
-                <iframe
+    <div class="h-full lg:bg-neutral-800 justify-around  md: bg-red-300 flex flex-col">
+        <div class="h-full flex flex-col mt-20">
+            <div >
+                <iframe class="lg:w-4/6 aspect-video sm:w-full"
                     src="https://player.twitch.tv/?channel=esl_csgo&parent=127.0.0.1&parent=localhost"
-                    height="720"
-                    width="100%"
                     allowfullscreen="true">
                 </iframe>
             </div>
 
-            <div class="grid grid-cols-4 gap-4 mt-4 p-12">
-                <div><img src="images/twitter.png" alt="twitter"/></div>
-                <div><img src="images/facebook.png" alt="facebook"/></div>
-                <div><img src="images/discord.png" alt="discord"/></div>
-                <div><img src="images/youtube.png" alt="youtube"/></div>
-                <div><img src="images/design.png" alt="design"/></div>         
+            <div>
+                <iframe class="lg:fixed right-0 bottom-0 h-[calc(100%_-_66px)]" id="twitch-chat-embed "
+                src="https://www.twitch.tv/embed/akito_soma/chat?darkpopout&parent=127.0.0.1&parent=localhost"
+                width="340px">
+                
+                </iframe>
             </div>
             
         </div>
-        <div>
-            <iframe class="fixed right-0 bottom-0 h-[calc(100%_-_66px)]" id="twitch-chat-embed "
-            src="https://www.twitch.tv/embed/akito_soma/chat?darkpopout&parent=127.0.0.1&parent=localhost"
-            height="100%"
-            width="330">
-            </iframe>
+
+        <div class="lg:w-4/6 sm:w-full grid grid-cols-3 gap-4 mt-4 p-12">
+            <div><img src="images/twitter.png" alt="twitter"/></div>
+            <div><img src="images/facebook.png" alt="facebook"/></div>
+            <div><img src="images/discord.png" alt="discord"/></div>
+            <div><img src="images/youtube.png" alt="youtube"/></div>
+            <div><img src="images/design.png" alt="design"/></div>         
         </div>
     </div>
     
